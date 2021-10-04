d8c5bb7e585144b917dc82daee0b392faf8e1844
rushikesh Pawar <rushikesh.pawarms@gmail.com>
2021-10-04 22:26:17 +0530

    add all fles

---
+++ script.js
@@ -0,0 +1,102 @@
+
+ const $ = require("jquery");
+ let db;
+ let lsc;
+
+$(document).ready(function () {
+   $(".cell").on("click",function(){
+       
+       let rowid=Number($(this).attr("rid"));
+       let colid=Number($(this).attr("cid"));
+       let celladd=String.fromCharCode(65+colid)+(rowid+1);
+       $("#address").val(celladd);
+       let cellobject=db[rowid][colid];
+       $("#formula").val(cellobject.formula);
+       
+
+   })
+    $("#formula").on("blur", function(){
+        let formula=$(this).val();
+        //getting value of formala;
+        
+      
+      // db upadte
+        let address=$("#address").val();
+        let {rowid,colid}=getids(address);
+        let cellobject=db[rowid][colid];
+        if(cellobject.formula!=formula)
+        {
+            let value=solvef(formula);
+            cellobject.value=value+"";
+            cellobject.formula=formula;
+            //UI update
+            $(lsc).text(value);
+        }
+      
+        })
+
+        function solvef(formula)
+        {
+            let fcmps=formula.split(" ");
+            for(let i=0;i<fcmps.length;i++)
+            {
+                let fcomp=fcmps[i];
+                let cellname=fcomp[0];
+                if(cellname>='A' && cellname<='Z')
+                {
+                    let {rowid, colid}=getids(fcomp);
+                    
+                    let cellobject=  db[rowid][colid];
+                    let value=cellobject.value;
+                    formula=formula.replace(fcomp, value);
+                }
+            }
+            let value=eval(formula);
+            return value;
+        }
+
+        function getids(fcomp)
+        {
+            let colid=fcomp.charCodeAt(0)-65;
+            let rowid=Number(fcomp.substring(1))-1;
+            return {rowid:rowid,colid:colid};
+        }
+
+
+
+//this will give update value at your databse
+$(".cell").on("blur",function(){
+    lsc=$(this);
+    let val=$(this).text();
+    let rowid=Number($(this).attr("rid"));
+    let colid=Number($(this).attr("cid"));
+    let cellobject=db[rowid][colid];
+   if(cellobject.value!=val)
+   {
+    cellobject.value=val;
+   }
+})
+
+   // this will create a DB for store cell data;
+   function init()
+   {
+   db=[];
+   for(let i=0;i<100;i++)
+   {
+       row=[];
+       for(let j=0;j<26;j++)
+       {
+           let cellad=String.fromCharCode(65+j)+(i+1);
+               cellobject={
+                   name:cellad,
+                   value:"",
+                   formula:""
+                       }
+           row.push(cellobject);
+       }
+       db.push(row);
+   }
+   console.log(db);
+   }
+   init();
+})
---
+++ test.js
@@ -0,0 +1,5 @@
+let fcomp="A1";
+let colid=fcomp.charCodeAt(0)-65;
+let rowid=Number(fcomp.substring(1))-1;
+console.log(rowid);
+console.log(colid);
